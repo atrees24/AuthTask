95add7554e603cdcc0211d4b58ed51789211edd6
Ahmed <atrees.ahmed5544@gmail.com>
2024-11-21 19:52:57 +0200

    Add project files.

---
+++ AuthTask/Models/Context/AuthDb_Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthTask.Models.Context
+{
+    public class AuthDb_Context:IdentityDbContext
+    {
+        public AuthDb_Context(DbContextOptions<AuthDb_Context> options):base(options) { }
+        
+    }
+}
